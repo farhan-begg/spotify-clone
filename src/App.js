6fbb99580f168bb7b75de35a9fd20bd919777bd8
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-01-29 23:15:28 -0800

    successfully printed user object

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import './App.css';
 import Login from './Login';
 import { useEffect, useState } from 'react';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js"
 
+// instance of spotify
+const spotify = new SpotifyWebApi();
 
 function App() {
-
   const [token, setToken ] =  useState(null)
-
-
   // Run code based on a given condition
   useEffect(() => {
     // grab the token from url 
     const hash = getTokenFromUrl()
     // clears token
     window.location.hash = ""
-
     const _token = hash.access_token;
-
     if (_token){
       setToken(_token)
+      // sets access token helps connect spotify to react
+      spotify.setAccessToken(_token)
+      // async function gets user accounts 
+      spotify.getMe().then(user => {
+        console.log('***********userrrrrr', user)
+      })
     }
-
-    console.log("I have a token", token)
+    console.log("I have a token ******", token)
   }, []);
-
-
 
   return (
     <div className="app">
