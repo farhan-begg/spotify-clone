103cb96ef279990b6d441d01e58ac2627f14db44
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-01-29 20:21:54 -0800

    login page working

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login';
+
+
+// client id :====== bbded4a98bc34320916f53a2f7983f73
+function App() {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
